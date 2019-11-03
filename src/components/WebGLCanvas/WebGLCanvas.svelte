1f0f1840a37173bee929b7a8a0c799dd81b03447
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2019-11-03 15:40:21 +0100

    [Update] Use https://tympanus.net/codrops/2019/09/25/mouse-flowmap-deformation-with-ogl/ in svelte compoent

--- src/components/WebGLCanvas/WebGLCanvas.svelte
+++ src/components/WebGLCanvas/WebGLCanvas.svelte
@@ -1,79 +1,219 @@
 <script>
-	import {Renderer, Camera, Program, Mesh, Box, Geometry, Flowmap} from 'ogl';
-	import { onMount } from 'svelte';
+	import { Renderer, Camera, Vec2, Vec4, Program, Mesh, Box, Texture, Geometry, Flowmap } from 'ogl'
+	import { onMount } from 'svelte'
 
 	let name = 'WebGLCanvas';
 	let renderer = null;
 	let geometry = null;
 	let program = null;
-	let mesh = null;
+	let flowmap = null;
+  let mesh = null;
+  let aspect = null;
+  let mouse = null;
+  let velocity = null;
+  let gl = null;
+  let lastTime = null;
+  let lastMouse = null;
+
+	const imgSize = [1250, 833];
 
 	function initWebGL() {
-		console.log('init')
 
-		renderer = new Renderer({
-			width: window.innerWidth,
-			height: window.innerHeight,
-		});
-		
-		const gl = renderer.gl;
-		document.body.appendChild(gl.canvas);
-
-		// Triangle that covers viewport, with UVs that still span 0 > 1 across viewport
-		geometry = new Geometry(gl, {
-			position: {size: 2, data: new Float32Array([-1, -1, 3, -1, -1, 3])},
-			uv: {size: 2, data: new Float32Array([0, 0, 2, 0, 0, 2])},
-		});
-
-		program = new Program(gl, {
-			vertex: `
-				attribute vec2 uv;
-				attribute vec2 position;
-
-				varying vec2 vUv;
-
-				void main() {
+		const vertex = `
+			attribute vec2 uv;
+			attribute vec2 position;
+			varying vec2 vUv;
+			void main() {
 					vUv = uv;
 					gl_Position = vec4(position, 0, 1);
-				}
-			`,
-			fragment: `
-				precision highp float;
+			}
+		`
 
-				uniform float uTime;
+		const fragment = `
+			precision highp float;
+			precision highp int;
+			uniform sampler2D tWater;
+			uniform sampler2D tFlow;
+			uniform float uTime;
+			varying vec2 vUv;
+			uniform vec4 res;
 
-				varying vec2 vUv;
+			void main() {
 
-				void main() {
-					gl_FragColor.rgb = vec3(0.8, 0.7, 1.0) + 0.3 * cos(vUv.xyx + uTime);
-					gl_FragColor.a = 1.0;
-				}
-			`,
-			uniforms: {
-				uTime: {value: 0},
-			},
-		})
+				// R and G values are velocity in the x and y direction
+				// B value is the velocity length
+				vec3 flow = texture2D(tFlow, vUv).rgb;
 
-		console.log('programi')
-		console.log('program', program)
+				vec2 uv = .5 * gl_FragCoord.xy / res.xy ;
+				vec2 myUV = (uv - vec2(0.5))*res.zw + vec2(0.5);
+				myUV -= flow.xy * (0.15 * 0.7);
 
-		mesh = new Mesh(gl, {geometry, program});
+				vec3 tex = texture2D(tWater, myUV).rgb;
 
-		requestAnimationFrame(update);
-	}
-	
-    function update(t) {
-        requestAnimationFrame(update);
+				gl_FragColor = vec4(tex.r, tex.g, tex.b, 1.0);
+		}`
 
-        program.uniforms.uTime.value = t * 0.001;
+    renderer = new Renderer({ dpr: 2 });
 
-        // Don't need a camera if camera uniforms aren't required
-        renderer.render({scene: mesh});
-	}
-	
+    gl = renderer.gl;
+    document.body.appendChild(gl.canvas);
+
+    // Variable inputs to control flowmap
+    aspect = 1;
+    mouse = new Vec2(-1);
+    velocity = new Vec2();
+
+    flowmap = new Flowmap(gl);
+
+    // Triangle that includes -1 to 1 range for 'position', and 0 to 1 range for 'uv'.
+    geometry = new Geometry(gl, {
+      position: {
+        size: 2,
+        data: new Float32Array([-1, -1, 3, -1, -1, 3])
+      },
+      uv: { size: 2, data: new Float32Array([0, 0, 2, 0, 0, 2]) }
+    });
+
+    const texture = new Texture(gl, {
+      minFilter: gl.LINEAR,
+      magFilter: gl.LINEAR
+    });
+
+    const img = new Image();
+    img.onload = () => (texture.image = img);
+    img.crossOrigin = "Anonymous";
+    img.src = "img/demo1.jpg";
+
+    let a1, a2;
+    var imageAspect = imgSize[1] / imgSize[0];
+    if (window.innerHeight / window.innerWidth < imageAspect) {
+      a1 = 1;
+      a2 = window.innerHeight / window.innerWidth / imageAspect;
+    } else {
+      a1 = (window.innerWidth / window.innerHeight) * imageAspect;
+      a2 = 1;
+    }
+
+    program = new Program(gl, {
+      vertex,
+      fragment,
+      uniforms: {
+        uTime: { value: 0 },
+        tWater: { value: texture },
+        res: {
+          value: new Vec4(window.innerWidth, window.innerHeight, a1, a2)
+        },
+        img: { value: new Vec2(imgSize[0], imgSize[1]) },
+        // Note that the uniform is applied without using an object and value property
+        // This is because the class alternates this texture between two render targets
+        // and updates the value property after each render.
+        tFlow: flowmap.uniform
+      }
+    });
+
+    mesh = new Mesh(gl, { geometry, program })
+
+    window.addEventListener("resize", resize, false);
+    resize();
+
+    // Create handlers to get mouse position and velocity
+    const isTouchCapable = "ontouchstart" in window;
+    if (isTouchCapable) {
+      window.addEventListener("touchstart", updateMouse, false);
+      window.addEventListener("touchmove", updateMouse, { passive: false });
+    } else {
+      window.addEventListener("mousemove", updateMouse, false);
+    }
+
+    lastTime;
+    lastMouse = new Vec2();
+
+    requestAnimationFrame(update);
+  }
+
+  function resize() {
+    let a1, a2;
+    var imageAspect = imgSize[1] / imgSize[0];
+    if (window.innerHeight / window.innerWidth < imageAspect) {
+      a1 = 1;
+      a2 = window.innerHeight / window.innerWidth / imageAspect;
+    } else {
+      a1 = (window.innerWidth / window.innerHeight) * imageAspect;
+      a2 = 1;
+    }
+    mesh.program.uniforms.res.value = new Vec4(
+      window.innerWidth,
+      window.innerHeight,
+      a1,
+      a2
+    );
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    aspect = window.innerWidth / window.innerHeight;
+  }
+
+
+  function updateMouse(e) {
+    e.preventDefault();
+    if (e.changedTouches && e.changedTouches.length) {
+      e.x = e.changedTouches[0].pageX;
+      e.y = e.changedTouches[0].pageY;
+    }
+    if (e.x === undefined) {
+      e.x = e.pageX;
+      e.y = e.pageY;
+    }
+    // Get mouse value in 0 to 1 range, with y flipped
+    mouse.set(e.x / gl.renderer.width, 1.0 - e.y / gl.renderer.height);
+    // Calculate velocity
+    if (!lastTime) {
+      // First frame
+      lastTime = performance.now();
+      lastMouse.set(e.x, e.y);
+    }
+
+    const deltaX = e.x - lastMouse.x;
+    const deltaY = e.y - lastMouse.y;
+
+    lastMouse.set(e.x, e.y);
+
+    let time = performance.now();
+
+    // Avoid dividing by 0
+    let delta = Math.max(10.4, time - lastTime);
+    lastTime = time;
+    velocity.x = deltaX / delta;
+    velocity.y = deltaY / delta;
+    // Flag update to prevent hanging velocity values when not moving
+    velocity.needsUpdate = true;
+  }
+
+
+  function update(t) {
+    requestAnimationFrame(update);
+    // Reset velocity when mouse not moving
+    if (!velocity.needsUpdate) {
+      mouse.set(-1);
+      velocity.set(0);
+    }
+
+    velocity.needsUpdate = false;
+
+    // Update flowmap inputs
+    flowmap.aspect = aspect;
+    flowmap.mouse.copy(mouse);
+
+    // Ease velocity input, slower when fading out
+    flowmap.velocity.lerp(velocity, velocity.len ? 0.15 : 0.1);
+    flowmap.update();
+    program.uniforms.uTime.value = t * 0.01;
+    renderer.render({ scene: mesh });
+  }
+
 	onMount(() => {
 		initWebGL()
-	});
+  });
+
 </script>
 
 <style lang="sass">
